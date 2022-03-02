b5fe64a4b32d80fc7deb41d6a2659197c101f74a
Michelle Caraballo <caraballomp@yahoo.com>
2022-03-01 21:52:58 -0800

    commit tree 707c2351418b15026bd4f138002cb21631561445

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+// function findMatching(elements){
+//     return elements >10;
+// }
+// const filterNum = elements.filter(findMatching)
+// console.log(filterNum);
+
+function findMatching(drivers, name){
+    return drivers.filter(nam => nam.toUpperCase() === name.toUpperCase()) 
+}
+
+function fuzzyMatch (drivers,letters){
+    return drivers.filter(nam => nam.substring(0, 2) === letters)
+}
+
+function matchName(drivers, name){
+return drivers.filter(nam => nam.name === name)
+}
